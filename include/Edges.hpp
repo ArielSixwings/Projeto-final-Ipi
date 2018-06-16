e97d11bc546daace471add2aedab073705c87dbc
Ariel Serafim <arielserafim@gmail.com>
2018-06-16 00:36:34 -0300

    primeira modularização

---
+++ include/Edges.hpp
@@ -0,0 +1,12 @@
+#ifndef EDGES_H
+#define EDGES_H
+
+#include <opencv2/opencv.hpp>
+#include "iostream"
+ 
+using namespace cv;
+using namespace std;
+
+void TakeEdges(Mat& Image, Mat& Edges);
+
+#endif
