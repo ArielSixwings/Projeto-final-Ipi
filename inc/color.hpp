459cb0d43a85e36c50ea096071f1630e9ac753ae
luigimaia99 <luigimaia99@gmail.com>
2018-06-16 02:18:52 -0300

    Novo Módulo

---
+++ inc/color.hpp
@@ -0,0 +1,24 @@
+/** @file color.hpp
+ @author
+ @date
+ @brief
+ @copyright GNU Public License.
+*/
+
+#ifndef COLOR_HPP_
+#define COLOR_HPP_
+
+namespace color {
+
+/** @brief
+ *
+ @param
+ @tparam
+ @return
+ @pre
+ @post
+*/
+void func();
+
+} // namespace color
+#endif // COLOR_HPP_
---
+++ src/color.cpp
@@ -0,0 +1,17 @@
+/** @file color.cpp
+ @author
+ @date
+ @brief
+ *
+ @copyright GNU Public License.
+*/
+
+#include "color.hpp"
+
+namespace color {
+
+void func() {
+  return;
+} // func()
+
+} // namespace color
