ff54ad42621e326c277da13a79509882060b94b1
Ariel Serafim <arielserafim@gmail.com>
2018-06-15 21:14:43 -0300

    iniciei o repositorio

---
+++ src/Edges.cpp
@@ -0,0 +1,11 @@
+void TakeEdges(Mat& Image, Mat& Edges){
+
+	Mat grayImage, edge, draw,dst;
+	cvtColor(Image, grayImage, CV_BGR2GRAY);
+	medianBlur( grayImage, dst, 7 );
+	Canny( dst, edge, 50, 150, 3);
+	edge.convertTo(draw, CV_8U);
+	namedWindow("image", CV_WINDOW_AUTOSIZE);
+	imshow("image", draw);
+	waitKey(0); 	
+}
