e7758f1d001582f4b6bde088d1836ad439569f70
Ariel Serafim <arielserafim@gmail.com>
2018-06-16 02:01:49 -0300

    final dessa madruga

--- src/Edges.cpp
+++ src/Edges.cpp
@@ -3,10 +3,15 @@
 namespace edges {
 
 cv::Mat TakeEdges(cv::Mat& image,int lowThreshold, int range){
-	//lowThreshold, lowThreshold*ratio
 	cv::Mat grayImage,edge,dst;
+	
+	/*convert to gray scale*/
 	cv::cvtColor(image, grayImage, CV_BGR2GRAY);
+	
+	/*apply median blur with a 7x7 kernel*/
 	cv::medianBlur( grayImage, dst, 7 );
+	
+	/*apply Canny edge detector*/	
 	cv::Canny( dst, edge, lowThreshold, (lowThreshold+range), 3);
 	edge.convertTo(edge, CV_8U);
 	cv::namedWindow("image", CV_WINDOW_AUTOSIZE);
