89dbb1235b049c0787acec6a26b4843a3a9ad6dd
luigimaia99 <luigimaia99@gmail.com>
2018-06-17 18:54:01 -0300

    Basic color module ended.

--- Tests/test_color.cpp
+++ Tests/test_color.cpp
@@ -6,13 +6,21 @@
 TEST_CASE ("All") {
   SECTION ("Lena") {
     cv::Mat img = cv::imread("../lena.jpg");
-    img = color::blockColorRegions(img);
     cv::imshow("Lena", img);
+    img = color::blockColorRegions(img);
+    cv::imshow("Color blocked Lena", img);
   }
   SECTION ("CR7") {
     cv::Mat img = cv::imread("../CR7.png");
-    img = color::blockColorRegions(img);
     cv::imshow("CR7", img);
-    cv::waitKey();
+    img = color::blockColorRegions(img);
+    cv::imshow("Color blocked CR7", img);
   }
+  SECTION ("Cat") {
+    cv::Mat img = cv::imread("../Cat.jpg");
+    cv::imshow("Cat", img);
+    img = color::blockColorRegions(img);
+    cv::imshow("Color blocked Cat", img);
+  }
+  cv::waitKey();
 } // TEST_CASE
--- src/color.cpp
+++ src/color.cpp
@@ -9,35 +9,45 @@
 #include "color.hpp"
 
 namespace color {
+
 namespace bilateral_filter {
-const int INTERATIONS = 14;
-const int NEIGHBORHOOD = 4;
-const double SIGMA_COLOR = 32;
-const double SIGMA_SPACE = 4;
+const int ITERATIONS = 20;
+const int NEIGHBORHOOD = 12;
+const double SIGMA_COLOR = 16;
+const double SIGMA_SPACE = 5;
 } // namespace bilateralFilter
+
 namespace median {
-  const int KERNEL = 7;
+
+const int KERNEL = 7;
+
 } // namespace median
-const int REDUCTION_FACTOR = 20;
+const int COLOR_FACTOR = 10 ; /// COLOR_FACTOR = 256/num_colors;
 
 cv::Mat blockColorRegions(const cv::Mat& src) {
   cv::Mat filtered;
-  for (int i = 0; i < bilateral_filter::INTERATIONS; ++i) {
-    cv::bilateralFilter(src, filtered, bilateral_filter::NEIGHBORHOOD,
+  cv::pyrDown(src, filtered, cv::Size(src.cols/2, src.rows/2));
+  for (int i = 0; i < bilateral_filter::ITERATIONS; ++i) {
+    cv::Mat aux; // The funtion bilateralFilter doesn't work inplace, that is
+                 //why I need to create and destroy this aux varible.
+    cv::bilateralFilter(filtered, aux, bilateral_filter::NEIGHBORHOOD,
                         bilateral_filter::SIGMA_COLOR,
                         bilateral_filter::SIGMA_SPACE);
+    filtered = aux.clone();
   } // for (i)
+  cv::pyrUp(filtered, filtered, cv::Size(src.cols, src.rows));
   cv::medianBlur(filtered, filtered, median::KERNEL);
-  // Requantize the color palette
   for (int r = 0; r < filtered.rows; ++r) {
     for (int c = 0; c < filtered.cols; ++c) {
       auto pixel = filtered.at<cv::Vec3b>(r, c);
-      pixel[0] = int(pixel[0] / REDUCTION_FACTOR) * REDUCTION_FACTOR;
-      pixel[1] = int(pixel[1] / REDUCTION_FACTOR) * REDUCTION_FACTOR;
-      pixel[2] = int(pixel[2] / REDUCTION_FACTOR) * REDUCTION_FACTOR;
+      pixel[0] = int(pixel[0] / COLOR_FACTOR) * COLOR_FACTOR + COLOR_FACTOR / 2;
+      pixel[1] = int(pixel[1] / COLOR_FACTOR) * COLOR_FACTOR + COLOR_FACTOR / 2;
+      pixel[2] = int(pixel[2] / COLOR_FACTOR) * COLOR_FACTOR + COLOR_FACTOR / 2;
       filtered.at<cv::Vec3b>(r, c) = pixel;
     } // for (r)
   }  // for (c)
+  // Requantize the color palette
   return filtered ;
 } // blockColorRegions()
+
 } // namespace color
